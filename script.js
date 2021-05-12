9bb3228757e64fcb6e30af695741492b167fda6a
Glitch (glitch-hello-website) <none>
2021-05-12 10:42:09 +0000

    🐪🐫 Checkpoint ./script.js:13690106/11712 ./style.css:13690106/2343

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // DOM element where the Timeline will be attached
-var container = document.getElementById('visualization');
+var container = document.getElementById("visualization");
 
 // create a DataSet
 var data = new vis.DataSet();
 
 // add items
-data.add([{
+data.add([
+  {
     id: 5,
-    content: 'Start!',
+    content: "Start!",
     start: Date.now()
   }
 ]);
@@ -22,31 +23,34 @@ var timeline = new vis.Timeline(container, data, options);
 window.addItem = function() {
   // Create a DataSet (allows two way data-binding)
   var id = Date.now();
-  var url = "http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3";
-  var html = "<div id='wave" + id + "'>audio</div>"
-  var items = [{
-    id: 7,
-    content: html,
-    start: new Date()
-  }];
+  var url =
+    "http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3";
+  var html = "<div id='wave" + id + "'>audio</div>";
+  var items = [
+    {
+      id: 7,
+      content: html,
+      start: new Date()
+    }
+  ];
   timeline.moveTo(new Date(), {
     animation: false
   });
 
   data.add(items);
   var wave = WaveSurfer.create({
-    container: '#wave' + id,
+    container: "#wave" + id,
     fillParent: true,
     autoCenter: true,
     mediaControls: true
   });
-  wave.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
-  wave.on('ready', function() {
+  wave.load(
+    "http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+  );
+  wave.on("ready", function() {
     wave.play();
   });
-
 };
-
 
 // REC CODE
 
@@ -54,9 +58,9 @@ var gumStream; //stream from getUserMedia()
 var rec; //Recorder.js object
 var input; //MediaStreamAudioSourceNode we'll be recording
 
-// shim for AudioContext when it's not avb. 
+// shim for AudioContext when it's not avb.
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext //audio context to help us record
+var audioContext; //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
@@ -67,7 +71,7 @@ recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
-var time = {}
+var time = {};
 
 function startRecording() {
   console.log("recordButton clicked");
@@ -81,7 +85,7 @@ function startRecording() {
   var constraints = {
     audio: true,
     video: false
-  }
+  };
 
   /*
     	Disable the record button until we get a success or fail from getUserMedia() 
@@ -89,51 +93,56 @@ function startRecording() {
 
   recordButton.disabled = true;
   stopButton.disabled = false;
-  pauseButton.disabled = false
+  pauseButton.disabled = false;
 
   /*
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      console.log(
+        "getUserMedia() success, stream created, initializing Recorder.js ..."
+      );
 
-    /*
+      /*
     	create an audio context after getUserMedia is called
     	sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
     	the sampleRate defaults to the one set in your OS for your playback device
     */
-    audioContext = new AudioContext();
+      audioContext = new AudioContext();
 
-    //update the format 
-    document.getElementById("formats").innerHTML = "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz"
+      //update the format
+      document.getElementById("formats").innerHTML =
+        "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz";
 
-    /*  assign to gumStream for later use  */
-    gumStream = stream;
+      /*  assign to gumStream for later use  */
+      gumStream = stream;
 
-    /* use the stream */
-    input = audioContext.createMediaStreamSource(stream);
+      /* use the stream */
+      input = audioContext.createMediaStreamSource(stream);
 
-    /* 
+      /* 
     	Create the Recorder object and configure to record mono sound (1 channel)
     	Recording 2 channels  will double the file size
     */
-    rec = new Recorder(input, {
-      numChannels: 1
+      rec = new Recorder(input, {
+        numChannels: 1
+      });
+
+      //start the recording process
+      rec.record();
+
+      console.log("Recording started");
     })
-
-    //start the recording process
-    rec.record()
-
-    console.log("Recording started");
-
-  }).catch(function(err) {
-    //enable the record button if getUserMedia() fails
-    recordButton.disabled = false;
-    stopButton.disabled = true;
-    pauseButton.disabled = true
-  });
+    .catch(function(err) {
+      //enable the record button if getUserMedia() fails
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+      pauseButton.disabled = true;
+    });
 }
 
 function pauseRecording() {
@@ -144,9 +153,8 @@ function pauseRecording() {
     pauseButton.innerHTML = "Resume";
   } else {
     //resume
-    rec.record()
+    rec.record();
     pauseButton.innerHTML = "Pause";
-
   }
 }
 
@@ -173,53 +181,54 @@ function stopRecording() {
 }
 
 function createDownloadLink(blob) {
-	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
+  var url = URL.createObjectURL(blob);
+  var au = document.createElement("audio");
+  var li = document.createElement("li");
+  var link = document.createElement("a");
 
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
+  //name of .wav file to use during upload and download (without extendion)
+  var filename = new Date().toISOString();
 
-	//add controls to the <audio> element
-	au.controls = true
+  //add controls to the <audio> element
+  au.controls = true;
   au.autoplay = true;
-	au.src = url;
-  
+  au.src = url;
+
   var player = au;
 
-	//save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
+  //save to disk link
+  link.href = url;
+  link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
+  link.innerHTML = "Save to disk";
 
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
+  //add the new audio element to li
+  li.appendChild(au);
 
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//add the li element to the ol
-	//recordingsList.appendChild(li);
-  
-  console.log('html', player);
-  
-   var items = [{
-    id: Date.now(),
-    content: player,
-    start: time.start,
-    end: time.stop
-   }];
+  //add the filename to the li
+  li.appendChild(document.createTextNode(filename + ".wav "));
+
+  //add the save to disk link to li
+  li.appendChild(link);
+
+  //add the li element to the ol
+  //recordingsList.appendChild(li);
+
+  console.log("html", player);
+
+  var items = [
+    {
+      id: Date.now(),
+      content: player,
+      title: 'audio',
+      start: time.start,
+      end: time.stop
+    }
+  ];
   timeline.moveTo(time.start, {
     animation: false
   });
   //timeline.fit()
-  console.log(items)
+  console.log(items);
   data.add(items);
   time = {};
-  
 }
